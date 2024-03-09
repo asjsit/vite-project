a9ccd6f3ef20783a395d4990c9889270679fda46
asjsit <saroenkoanton0@gmail.com>
2024-03-09 18:14:05 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import styles from "./App.module.scss";
+import arrow from "./assets/arrow.svg";
 
 function App() {
   return (
-    <>
-      Hello world
-    </>
-  )
+    <div className="flex justify-center items-center h-screen max-[500px]:px-5">
+      <div className="flex flex-col p-8 border border-borderCustom rounded-3xl">
+        <h2 className="text-whiteCustom text-center font-medium text-3xl">
+          Регистрация аккаунта
+        </h2>
+        <div className="flex gap-5 mt-9">
+          <div className="flex flex-col gap-5">
+            <p className={styles.p}>Имя</p>
+            <input type="text" placeholder="Имя" className={styles.input} />
+          </div>
+          <div className="flex flex-col gap-5">
+            <p className={styles.p}>Фамилия</p>
+            <input type="text" placeholder="Фамилия" className={styles.input} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 mt-5">
+          <p className={styles.p}>Адрес электронной почты</p>
+          <input
+            type="text"
+            placeholder="Адрес электронной почты"
+            className={styles.input}
+          />
+        </div>
+        <div className="flex flex-col gap-5 mt-5">
+          <p className={styles.p}>Пароль</p>
+          <input type="text" placeholder="Пароль" className={styles.input} />
+        </div>
+        <p className={styles.agreement}>
+          Регистрируясь, вы соглашаетесь с условиями использования нашего
+          сервиса{" "}
+          <span className="text-greenCustom text-xl font-normal underline">
+            соглашение
+          </span>
+        </p>
+        <button className="border border-borderCustom py-3.5 rounded-xl mt-5 text-whiteCustom font-normal text-xl flex items-center justify-center gap-3.5">
+          <p>Продолжить</p>
+          <img src={arrow} alt="arrow" className="max-[500px]:hidden"/>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      'whiteCustom': "#E8EAED",
+      'bgCustom': '#131315',
+      'borderCustom': '#454545',
+      'inputText': '#939393',
+      'greenCustom': '#2EFF43'
+    },
+    extend: {}
+  },
+  plugins: [],
+};
